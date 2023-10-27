4cd13b587de602a3527353f4bad5c23701bbebad
EDITH_Coder <edithmashapo@gmail.com>
2023-10-27 21:53:18 +0200

    Add project files.

---
+++ Constructor1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor1
+{
+    internal class Car
+    {
+        public string Model;
+        //constructor
+        public Car(string model1) {
+            this.Model = model1;
+        }
+        static void Main(string[] args)
+        {
+            //ask the user to enter a model
+            Console.WriteLine("Enter a model");
+            string model= Console.ReadLine();
+
+            Console.WriteLine(" ");
+            //created an object
+            Car myCar = new Car(model);
+            Console.WriteLine("The car model is: "+ myCar.Model);
+
+            Console.ReadLine();
+
+        }
+    }
+}
